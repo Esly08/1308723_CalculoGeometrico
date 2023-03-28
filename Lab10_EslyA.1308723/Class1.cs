63d846dd5a506131257cbee55a466faf75b71955
Esly Ajsivinac <emasjivinac@correo.url.edu.gt>
2023-03-28 17:41:49 -0600

    Agregar archivos de proyecto.

---
+++ Lab10_EslyA.1308723/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10_EslyA._1308723
+{
+    public class Class1
+    {
+        public static void Main()
+        {
+            
+            Console.WriteLine("Bienvenido a los cálculos geométricos");
+            Console.WriteLine("");
+            Console.WriteLine("Por favor ingrese el radio del círculo");
+            double _Rad1 = double.Parse(Console.ReadLine());
+            Circulo objCirculo = new Circulo(_Rad1);
+
+            double _per = 0.0;
+            double _ar = 0.0;
+            double _vol = 0;
+
+            objCirculo._CalcularGeometria(ref _per, ref _ar, ref _vol);
+
+            Console.WriteLine("El perímetro es: " + _per);
+            Console.WriteLine("El área es: " + _ar);
+            Console.WriteLine("El volumen es: " + _vol);
+
+        }
+    }
+}
---
+++ Lab10_EslyA.1308723/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Lab10_EslyA._1308723
+{
+    public class Circulo
+    {         
+        private double _radio;
+        
+        public Circulo(double _radio)
+        {
+            this._radio = _radio;
+        }
+
+        private double _Perimetro()
+        {
+            return 2 * Math.PI * _radio;
+        }
+        
+        private double _Area()
+        {
+            return Math.PI * (Math.Pow(_radio, 2));
+        }
+
+        private double _volumen()
+        {
+            return 4 * Math.PI * (Math.Pow(_radio, 3)) % 3;
+        }
+
+        public void _CalcularGeometria(ref double unPerimetro, ref double unArea, ref double unVolumen)
+        {
+            unPerimetro = _Perimetro();
+            unArea = _Area();
+            unVolumen = _volumen();
+        }
+    }
+}
